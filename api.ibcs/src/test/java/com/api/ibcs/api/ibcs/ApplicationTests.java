5457a39eb08989016909626ca4fb8943145987df
Felipe Rocha <felperok20@hotmail.com>
2023-08-13 18:46:54 -0300

    Criando o projeto.

---
+++ api.ibcs/src/test/java/com/api/ibcs/api/ibcs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ibcs.api.ibcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
